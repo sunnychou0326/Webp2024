c3a2dd6e441604ee88200b674ad82ce7f0ff2944
sunnychou0326 <161412029+sunnychou0326@users.noreply.github.com>
2024-05-01 16:15:56 +0800

    Add files via upload

---
+++ lab4/react_login/src/App.jsx
@@ -0,0 +1,15 @@
+import CGU_Login from './cgu_login';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="card">
+      <div>
+        {CGU_Login()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
